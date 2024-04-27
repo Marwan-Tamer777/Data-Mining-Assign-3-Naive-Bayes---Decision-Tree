bf46899fe8d31b16d5d2b221ed8cfeebf1498363
Marwan-Tamer777 <marwanmarwan536@gmail.com>
2024-04-28 01:58:11 +0300

    Naive Bays base algortihm
    - Added the structure for the algorithm
    
    - Added logic for discrete attributes probability computing

--- Assignment3.py
+++ Assignment3.py
@@ -11,11 +11,50 @@ import easygui
 
 OriginData = pd.read_csv("diabetes_prediction_dataset.csv") #pd.read_csv(file_name)
 OriginData.dropna()
-
+print("TEST")
+print(OriginData[OriginData.columns[-1]].unique())
 # Get sample size and training to test distribution
 sample_size = 1 #float(easygui.enterbox("Enter Size of the sample data as fraction: "))
 data = OriginData.sample(frac=sample_size).sort_index()
 test_size = 0.25 #float(easygui.enterbox("Enter Size of the test data as fraction: "))
 trainData, testData = train_test_split(OriginData, test_size=test_size)
 
-print(trainData['smoking_history'].unique())
+# To implement naive bayes, We iterate over each test tuple.
+outputClassesList = OriginData[OriginData.columns[-1]].unique()
+for index,testTuple in testData.iterrows():
+    outputClassesScore = []
+
+    print(testTuple)
+    # And calculate the probability of it belonging to each possible output class
+    for outputClass in outputClassesList:
+        classifiedTrainData = trainData[trainData.iloc[:,-1:] == outputClass]
+        score = len(classifiedTrainData ) / len(trainData)
+        
+        # We do that by multiplying P(X|Ci) with each attribute probability
+        for column_name in trainData.columns.values:
+
+            # If it is continuous we get mean and standard deviation to calculate probability 
+            attributeUniques = classifiedTrainData[column_name].unique()
+            if len(attributeUniques)>10:
+                x = 0
+            # If the attribute is discrete we compute score 
+            else: 
+                matchedTuplesLen = len(classifiedTrainData[classifiedTrainData[column_name] == testTuple[column_name]])
+                if matchedTuplesLen == 0:
+                    score *= ( 1 / (len(classifiedTrainData) + len(classifiedTrainData[column_name].unique())) )
+                else:
+                    score*= (matchedTuplesLen / len(classifiedTrainData))
+
+        outputClassesScore.append({outputClass:score})
+        print(str(outputClass) + " : " + str(score))
+
+    # After adding all the outputclasses score in the list we find the biggest one
+    minNum = -1
+    predictedOutputClass = ''
+    for outputDic in outputClassesScore:
+        for key, val in outputDic.items():
+            if val > minNum:
+                minNum = val
+                predictedOutputClass = key
+    
+    print( "The Predicted Class is: " + str(predictedOutputClass))
