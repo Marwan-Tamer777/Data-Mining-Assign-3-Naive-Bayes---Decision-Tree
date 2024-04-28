64d3db75232bfe1b4c51e9dae1b351165068af59
Marwan-Tamer777 <marwanmarwan536@gmail.com>
2024-04-28 07:39:31 +0300

    finished the naïve Bayes
    - Added continuous attribute probability logic
    
    - Fixed issues with score logic
    
    - Accuracy of 60%

--- Assignment3.py
+++ Assignment3.py
@@ -1,6 +1,7 @@
 # importing libraries
 import pandas as pd
 import numpy as np
+import math
 from util import colored, normalise0to100,calcDistance
 from sklearn.model_selection import train_test_split
 from tkinter import *
@@ -11,32 +12,38 @@ import easygui
 
 OriginData = pd.read_csv("diabetes_prediction_dataset.csv") #pd.read_csv(file_name)
 OriginData.dropna()
-print("TEST")
-print(OriginData[OriginData.columns[-1]].unique())
 # Get sample size and training to test distribution
-sample_size = 1 #float(easygui.enterbox("Enter Size of the sample data as fraction: "))
+sample_size = 0.01 #float(easygui.enterbox("Enter Size of the sample data as fraction: "))
 data = OriginData.sample(frac=sample_size).sort_index()
 test_size = 0.25 #float(easygui.enterbox("Enter Size of the test data as fraction: "))
-trainData, testData = train_test_split(OriginData, test_size=test_size)
+trainData, testData = train_test_split(data, test_size=test_size)
 
 # To implement naive bayes, We iterate over each test tuple.
 outputClassesList = OriginData[OriginData.columns[-1]].unique()
+print(len(trainData))
+correctPreds = 0
+falsePreds = 0
 for index,testTuple in testData.iterrows():
     outputClassesScore = []
 
-    print(testTuple)
+    # print(testTuple)
     # And calculate the probability of it belonging to each possible output class
     for outputClass in outputClassesList:
-        classifiedTrainData = trainData[trainData.iloc[:,-1:] == outputClass]
+        classifiedTrainData = trainData[trainData["diabetes"] == outputClass]
+        classifiedTrainData = classifiedTrainData.drop(["diabetes"], axis = 1)
         score = len(classifiedTrainData ) / len(trainData)
-        
         # We do that by multiplying P(X|Ci) with each attribute probability
-        for column_name in trainData.columns.values:
+        for column_name in classifiedTrainData.columns.values:
 
             # If it is continuous we get mean and standard deviation to calculate probability 
             attributeUniques = classifiedTrainData[column_name].unique()
+
             if len(attributeUniques)>10:
-                x = 0
+                mean = classifiedTrainData[column_name].mean()
+                std = classifiedTrainData[column_name].std()
+                probablity = (math.e ** (-(testTuple[column_name]- mean)**2/2*std**2) ) / (math.sqrt(2*math.pi) * std )
+                if probablity != 0:
+                    score *= probablity
             # If the attribute is discrete we compute score 
             else: 
                 matchedTuplesLen = len(classifiedTrainData[classifiedTrainData[column_name] == testTuple[column_name]])
@@ -48,7 +55,7 @@ for index,testTuple in testData.iterrows():
         outputClassesScore.append({outputClass:score})
         print(str(outputClass) + " : " + str(score))
 
-    # After adding all the outputclasses score in the list we find the biggest one
+    # After adding all the output classes score in the list we find the biggest one
     minNum = -1
     predictedOutputClass = ''
     for outputDic in outputClassesScore:
@@ -58,3 +65,9 @@ for index,testTuple in testData.iterrows():
                 predictedOutputClass = key
     
     print( "The Predicted Class is: " + str(predictedOutputClass))
+    if predictedOutputClass != testTuple.iloc[-1]:
+        falsePreds+=1
+    else:
+        correctPreds+=1
+
+print("Success Ratio is: " + str(correctPreds/(correctPreds+falsePreds)))
